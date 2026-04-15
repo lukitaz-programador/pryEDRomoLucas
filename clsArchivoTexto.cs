8e02ba9968a45d5ba6fcd37b927ce9f061a02980
Lucas Romo <lucasromo.personal@gmail.com>
2026-04-15 09:21:43 -0300

    Refactor clsArchivoTexto for better clarity and functionality
    Refactor clsArchivoTexto class to improve file handling and method signatures.

--- clsArchivoTexto.cs
+++ clsArchivoTexto.cs
@@ -1,65 +1,47 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//agrego una libreria que me permite procesar archivos 
-using System.IO;
-using System.Windows.Forms;
 using System.Drawing.Text;
 using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+//agrego una libreria que me permite procesar archivos 
+using System.IO;
 
-namespace pryEDnuevoSANTINO
+
+namespace pryEDRomoL
 {
 
     internal class clsArchivoTexto
     {
         //creamos campo     
-        public string NomArchi = "Colores.txt";
-        public string NomArchi2 = "Meses.txt";
-        public string NomArchi3 = "Clientes.csv";
+        public string NomArchi;
 
-        public void Grabar()
-        {
-            //abrir archivo para escritura 
-            StreamWriter AD = new StreamWriter(NomArchi, true);
-            AD.WriteLine("hola!!");
-            AD.Close();
-
-        }
+        //public void Grabar()
+        //{
+        //abrir archivo para escritura 
+        //   StreamWriter AD = new StreamWriter(NomArchi, true);
+        //   AD.WriteLine("hola mundo!!");
+        //   AD.Close();
+        //}
 
         public void Grabar(string datos)
         {
             //abrir archivo para escritura 
             StreamWriter AD = new StreamWriter(NomArchi, true);
+            //se graban los datos en una linea
             AD.WriteLine(datos);
-            AD.Close();
-
-        }
-
-        public void Recorrer(ListBox cmbDatos)
-        {
-            //abrir archivo para lectura 
-            cmbDatos.Items.Clear();
-            string DatoLeido = "";
-            StreamReader AD = new StreamReader(NomArchi, true);
-            DatoLeido = AD.ReadLine();
-
-
-            while (DatoLeido != null)
-            {
-                //aca van las lineas de codigo que hagan falta
-                cmbDatos.Items.Add(DatoLeido);
-                DatoLeido = AD.ReadLine();
-            }
+            //se cierra el archivo de escritura
             AD.Close();
         }
 
         public void Recorrer(ComboBox cmbDatos)
         {
-            //abrir archivo para lectura 
+            //abrir archivo para lectura
+            string DatoLeido;
             cmbDatos.Items.Clear();
-            string DatoLeido = "";
             StreamReader AD = new StreamReader(NomArchi, true);
             DatoLeido = AD.ReadLine();
 
@@ -70,14 +52,36 @@ namespace pryEDnuevoSANTINO
                 cmbDatos.Items.Add(DatoLeido);
                 DatoLeido = AD.ReadLine();
             }
+            cmbDatos.SelectedIndex = 0;
             AD.Close();
         }
 
-        public void Recorrer(DataGridView cmbDatos)
+        public void Recorrer(ListBox lstDatos)
+        {
+            //abrir archivo para lectura
+            string DatoLeido;
+            lstDatos.Items.Clear();
+            StreamReader AD = new StreamReader(NomArchi, true);
+            //se lee la primera linea como en un registro
+            DatoLeido = AD.ReadLine();
+
+
+            while (DatoLeido != null)
+            {
+                //aca van las lineas de codigo que hagan falta
+                lstDatos.Items.Add(DatoLeido);
+                DatoLeido = AD.ReadLine();
+            }
+            //se cierra el archivo de lectura
+            lstDatos.SelectedIndex = 0;
+            AD.Close();
+        }
+
+        public void Recorrer(DataGridView dgvDatos)
         {
             //abrir archivo para lectura 
-            cmbDatos.Raws.+Clear();
-            string DatoLeido = "";
+            string DatoLeido;
+            dgvDatos.Rows.Clear();
             StreamReader AD = new StreamReader(NomArchi);
             DatoLeido = AD.ReadLine();
 
@@ -85,16 +89,16 @@ namespace pryEDnuevoSANTINO
             while (DatoLeido != null)
             {
                 //aca van las lineas de codigo que hagan falta
-                cmbDatos.Raws.Add(DatoLeido);
+                dgvDatos.Rows.Add(DatoLeido.Split(';'));
                 DatoLeido = AD.ReadLine();
             }
             AD.Close();
         }
 
         public void Grabar(string cod, string nom, string deu)
-        {              
-            StreamWriter AD = new StreamWriter(NomArchi3, true);
-            AD.WriteLine(cod+";"+nom+";"+deu);
+        {
+            StreamWriter AD = new StreamWriter(NomArchi, true);
+            AD.WriteLine(cod + ";" + nom + ";" + deu);
             AD.Close();
         }
 
@@ -105,4 +109,5 @@ namespace pryEDnuevoSANTINO
             AD.Close();
         }
     }
+}
 }
